c2150b8e8e491d85d29916911602ea5c0faa57bb
Eugen Vypasnyak <eugenvip@gmail.com>
2017-08-30 18:29:45 +0300

    second commit

--- src/HashMapOnTime.java
+++ src/HashMapOnTime.java
@@ -4,17 +4,17 @@ import java.util.HashMap;
 /**
  * Created by User on 18.08.2017.
  */
-public class HashMapOnTime
+public class HashMapOnTime<K, V>
 {
-    private HashMap<String, ArrayList> hashMapOnTime;
+    private HashMap hashMapOnTime = new HashMap<>();
     private int TTL = 5000;
 
     public HashMapOnTime()
     {
-        this.hashMapOnTime = new HashMap<>();
+        //this.hashMapOnTime = new HashMap<>();
     }
 
-    public void put(String key, String value) {
+    public void put(K key, V value) {
 
         ArrayList arrayList = new ArrayList();
         arrayList.add(System.currentTimeMillis());
@@ -22,11 +22,11 @@ public class HashMapOnTime
         this.hashMapOnTime.put(key, arrayList);
     }
 
-    public String get(String key){
-        ArrayList currentrray = this.hashMapOnTime.get(key);
-        Long timeOfValue = (Long)currentrray.get(0);
+    public <V>V get(K key){
+        ArrayList currentArray = (ArrayList) this.hashMapOnTime.get(key);
+        Long timeOfValue = (Long)currentArray.get(0);
         if (timeOfValue+TTL >= System.currentTimeMillis()){
-            return (String) currentrray.get(1);
+            return (V) currentArray.get(1);
         }else
         {
             return null;
